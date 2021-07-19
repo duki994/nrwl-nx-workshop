fc8be95a2d32141bd1070e5c63523bc2cb1442c0
Dusan K <duki994@gmail.com>
2021-07-19 09:57:09 +0200

    Initial

---
+++ apps/store/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    MatCardModule,
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
+    StoreUiSharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

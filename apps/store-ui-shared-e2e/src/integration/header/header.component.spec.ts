a0e0c30c5bd2a90713e0ba190c4032c7c81d9e72
Dusan K <duki994@gmail.com>
2021-07-20 09:57:58 +0200

    Lab11

--- apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
+++ apps/store-ui-shared-e2e/src/integration/header/header.component.spec.ts
@@ -1,6 +1,8 @@
 describe('store-ui-shared', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=headercomponent--primary&args=title;'));
+  beforeEach(() => cy.visit(
+    '/iframe.html?id=headercomponent--primary&args=title:BoardGameHoard'
+  ));
   it('should render the component', () => {
-    cy.get('bg-hoard-header').should('exist');
+    cy.get('bg-hoard-header').contains('BoardGameHoard');
   });
 });

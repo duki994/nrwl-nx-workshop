2fec3b18200cc58d857e54fc3dca835b9f4280e4
Dusan K <duki994@gmail.com>
2021-07-20 13:11:10 +0200

    Lab 13 - Updated workspace generator

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -1,8 +1,23 @@
 import { Tree, formatFiles, installPackagesTask } from '@nrwl/devkit';
 import { libraryGenerator } from '@nrwl/workspace/generators';
 
-export default async function (host: Tree, schema: any) {
-  await libraryGenerator(host, { name: schema.name });
+type LibrarySchema = Parameters<typeof libraryGenerator>[1];
+
+const checkNameHasUtil = (name: string) => name?.includes('util') ?? false;
+
+const mergeTags = (existingTags?: string, newTags?: string) => {
+  const trimmed = existingTags?.trim();
+  if (trimmed && newTags) {
+    return `${trimmed},${newTags}`;
+  }
+};
+
+export default async function (host: Tree, schema: LibrarySchema) {
+  const name = checkNameHasUtil(schema.name)
+    ? schema.name
+    : `util-${schema.name}`;
+
+  await libraryGenerator(host, { ...schema, name, tags: mergeTags(schema.tags, 'type:util') });
   await formatFiles(host);
   return () => {
     installPackagesTask(host);

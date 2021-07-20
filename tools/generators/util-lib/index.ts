5e8ca8683b707c95b933b880593fe6ea9ed34716
Dusan K <duki994@gmail.com>
2021-07-20 13:25:20 +0200

    Lab 13 - fix issues

--- tools/generators/util-lib/index.ts
+++ tools/generators/util-lib/index.ts
@@ -9,19 +9,33 @@ interface Schema extends LibrarySchema {
 
 const checkNameHasUtil = (name: string) => name?.includes('util') ?? false;
 
-const mergeTags = (existingTags?: string, newTags?: string) => {
+const mergeTags = (
+  existingTags?: string,
+  newTags?: string
+): string | undefined => {
   const trimmed = existingTags?.trim();
   if (trimmed && newTags) {
     return `${trimmed},${newTags}`.trim();
   }
+
+  return newTags;
 };
+
+const sanitizeName = (name?: string): string | undefined | null =>
+  name?.charAt(0) === '-' ? name.substr(1) : name;
 
 export default async function (host: Tree, schema: Schema) {
   const name = checkNameHasUtil(schema.name)
     ? schema.name
-    : `util-${schema.name}`;
+    : `util-${sanitizeName(schema.name)}`;
 
-  await libraryGenerator(host, { ...schema, name, tags: mergeTags(schema.tags, `scope:${schema.directory},type:util`) });
+  const libScheme: LibrarySchema = {
+    ...schema,
+    name,
+    tags: mergeTags(schema.tags, `scope:${schema.directory},type:util`),
+  };
+
+  await libraryGenerator(host, libScheme);
   await formatFiles(host);
   return () => {
     installPackagesTask(host);
